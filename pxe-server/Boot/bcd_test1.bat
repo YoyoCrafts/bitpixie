78d05fa54dd064fdf74cee5e81ff8f275320b197
xiaobai <yoyoyo20240@gmail.com>
2025-05-19 20:55:07 +0800

    1

--- pxe-server/Boot/bcd_test1.bat
+++ pxe-server/Boot/bcd_test1.bat
@@ -1,10 +1,21 @@
 @echo off
 
-
 bcdedit /export BCD_modded
 
+bcdedit /store BCD_modded /create /d "softreboot" /application bootsector>GUID.txt
+For /F "tokens=2 delims={}" %%i in (GUID.txt) do (set REBOOT_GUID=%%i)
+del GUID.txt
 
-bcdedit /store BCD_modded /set {default} winpe yes
+bcdedit /store BCD_modded /set {%REBOOT_GUID%} path "\shimx64.efi"
+bcdedit /store BCD_modded /set {%REBOOT_GUID%} device boot
+bcdedit /store BCD_modded /set {%REBOOT_GUID%} pxesoftreboot yes
+
+bcdedit /store BCD_modded /set {default} recoveryenabled yes
+bcdedit /store BCD_modded /set {default} recoverysequence {%REBOOT_GUID%}
+bcdedit /store BCD_modded /set {default} path "\"
+@REM bcdedit /store BCD_modded /set {default} winpe yes
+
+bcdedit /store BCD_modded /displayorder {%REBOOT_GUID%} /addlast
 
 
 setlocal
@@ -12,17 +23,20 @@ setlocal
 SET /P AREYOUSURE=Do you want to move the file to the SMB server on 10.13.37.100 (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
+move BCD_modded S:\BCD
 
-move BCD_modded S:\BCD_TEST1
+
 goto :EOF
 
 :END
 
-echo "BCD_modded file was not moved."
+move BCD_modded C:\BCD
+echo "BCD file was moved to C:\BCD"
 
 goto :EOF
 
 endlocal
 
 :EOF
+
 pause
