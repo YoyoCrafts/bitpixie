0773565512bafd449acd487f3a2c29b6f784aa02
xiaobai <yoyoyo20240@gmail.com>
2025-05-19 21:05:23 +0800

    1

--- pxe-server/Boot/bcd_test1.bat
+++ pxe-server/Boot/bcd_test1.bat
@@ -2,7 +2,7 @@
 
 bcdedit /export BCD_modded
 
-bcdedit /store BCD_modded /create /d "softreboot" /application bootsector>GUID.txt
+bcdedit /store BCD_modded /create /d "softreboot" /application startup>GUID.txt
 For /F "tokens=2 delims={}" %%i in (GUID.txt) do (set REBOOT_GUID=%%i)
 del GUID.txt
 
@@ -10,8 +10,8 @@ bcdedit /store BCD_modded /set {%REBOOT_GUID%} path "\shimx64.efi"
 bcdedit /store BCD_modded /set {%REBOOT_GUID%} device boot
 bcdedit /store BCD_modded /set {%REBOOT_GUID%} pxesoftreboot yes
 
-@REM bcdedit /store BCD_modded /set {default} recoveryenabled yes
-@REM bcdedit /store BCD_modded /set {default} recoverysequence {%REBOOT_GUID%}
+bcdedit /store BCD_modded /set {default} recoveryenabled yes
+bcdedit /store BCD_modded /set {default} recoverysequence {%REBOOT_GUID%}
 bcdedit /store BCD_modded /set {default} path "\\"
 bcdedit /store BCD_modded /set {default} winpe yes
 
--- pxe-server/Boot/create-bcd.bat
+++ pxe-server/Boot/create-bcd.bat
@@ -13,7 +13,7 @@ bcdedit /store BCD_modded /set {%REBOOT_GUID%} pxesoftreboot yes
 bcdedit /store BCD_modded /set {default} recoveryenabled yes
 bcdedit /store BCD_modded /set {default} recoverysequence {%REBOOT_GUID%}
 bcdedit /store BCD_modded /set {default} path "\\"
-@REM bcdedit /store BCD_modded /set {default} winpe yes
+bcdedit /store BCD_modded /set {default} winpe yes
 
 bcdedit /store BCD_modded /displayorder {%REBOOT_GUID%} /addlast
 
